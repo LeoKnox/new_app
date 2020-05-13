54ab48047652b286477d7c3d383f87252616d1f1
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-12 21:41:25 -0700

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'new dungeon app'
+@app.route('/index/<info>')
+def index(info):
+    return 'new %s app' % info
+
+@app.route('/room/<int:roomID>')
+def room(roomID):
+    return 'This is room %d' % roomID
+
+@app.route('/version/<float:verNo>')
+def version(verNo):
+    return 'Version Number %f' % verNo
 
 if __name__ == '__main__':
     app.run()
