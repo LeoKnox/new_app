c6d6788ae426142cad0a2698da33ed8e4de19883
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-12 22:11:30 -0700

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/index/<info>')
@@ -9,9 +9,16 @@ def index(info):
 def room(roomID):
     return 'This is room %d' % roomID
 
+@app.route('/floor/<int:roomID>')
+def floor(roomID):
+    if roomID == 5:
+        return redirect(url_for('index', info = 'dungeon'))
+    else:
+        return redirect(url_for('room', roomID = roomID))
+
 @app.route('/version/<float:verNo>')
 def version(verNo):
     return 'Version Number %f' % verNo
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
